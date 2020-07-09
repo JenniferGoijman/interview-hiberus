491019b8674ab0dd391bfd5e2b9cbf1f33883567
JenniferGoijman <jgoijman@gmail.com>
2020-07-09 12:36:11 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './App.css';
+
+import Login from './containers/Login/Login';
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
