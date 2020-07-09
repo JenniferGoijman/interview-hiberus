ae2f2cca845c44fd5b753b19875c9b22e6c95158
JenniferGoijman <jgoijman@gmail.com>
2020-07-09 16:06:56 +0200

    add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './App.css';
 
 import Login from './containers/Login/Login';
+import Register from './containers/Register/Register';
 
 function App() {
   return (
-    <Login></Login>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login} exact />
+          <Route path='/register' component={Register} exact />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
