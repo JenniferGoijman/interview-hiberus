d261334f47c2a2cfd1cef0499c2bb798a6ee99a1
JenniferGoijman <jgoijman@gmail.com>
2020-07-11 03:14:43 +0200

    add users to redux

---
+++ frontend/src/redux/actions/users.js
@@ -0,0 +1,38 @@
+import store from '../store';
+import axios from 'axios';
+
+export const register = async(user) => {
+    return axios.post('http://localhost:3001/users/register', user)
+}
+export const login = async(user) => {
+    const res = await axios.post('http://localhost:3001/users/login', user);
+    store.dispatch({
+        type: 'LOGIN',
+        payload: res.data.user
+    })
+    localStorage.setItem('authToken', res.data.token);
+    return res;
+}
+// export const logout = async() => {
+//     const res = await axios.get(API_URL + 'users/logout', {
+//         headers: {
+//             Authorization: "Bearer " + localStorage.getItem('authToken')
+//         }
+//     })
+//     localStorage.removeItem('authToken');
+//     store.dispatch({
+//         type: 'LOGOUT'
+//     })
+//     return res;
+// }
+export const getMyUser = async() => {
+    const res = await axios.get('http://localhost:3000/users/user', {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken')
+        }
+    });
+    store.dispatch({
+        type: 'SET_MY_USER',
+        payload: res.data
+    });
+}
